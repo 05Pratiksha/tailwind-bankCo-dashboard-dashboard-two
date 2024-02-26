74c00b72fb531eefda68a29aecbad5a99da087e5
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-02-26 17:38:02 +0530

    minor

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
-
+ 
 // Add PWA configuration
 const pwaConfig = {
   registerType: "autoUpdate",
